d7b141c00b4e08b18ab4f5ddf675f1fed8b28907
sunfuqiang <672249571@qq.com>
2018-03-02 14:04:42 +0800

    创建服务

---
+++ provider/src/main/java/com/sc/test/Test.java
@@ -0,0 +1,32 @@
+package com.sc.test;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * 2018/3/2
+ *
+ * @author sfq
+ * @version V1.0
+ **/
+@RestController
+public class Test {
+    private final Logger logger = Logger.getLogger(getClass());
+    @Autowired
+    private DiscoveryClient client;
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String index() {
+        List<ServiceInstance> instances = client.getInstances("hello-service");
+        for (int i = 0; i < instances.size(); i++) {
+            logger.info("/hello,host:" + instances.get(i).getHost() + ",service_id:" + instances.get(i).getServiceId());
+        }
+        return "Hello World";
+    }
+}
